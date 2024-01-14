8036ce4a69e232c4ddeb68c8e10386a5128566ac
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-01-14 16:27:15 +0100

    Add project files.

---
+++ Kata6/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Kata6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            return $"{numbers[0]}{numbers[1]}{numbers[2]} " +
+                $"{numbers[3]}{numbers[4]}{numbers[5]}-" +
+                $"{numbers[6]}{numbers[7]}{numbers[8]}{numbers[9]}";
+        }
+    }
+}
